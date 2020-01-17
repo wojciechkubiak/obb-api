4148c7f0ec4da46540ba6fe8cf28c8bf7052852b
Wojciech Kubiak <wgkubiak@gmail.com>
2020-01-17 19:27:32 +0100

    Database connection set up

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 import 'dotenv/config';
-
+const pool = require('../utility/connection')
 const { Router } = require('express');
 
 const router = Router();
 
+
+
 /* GET index page. */
-router.get('/', (req, res) => {
-  res.json({
-    title: process.env.WELCOME_MESSAGE
-  });
-});
+router.get('/', (req, res, next) => {
+  pool.query('SELECT * FROM names',
+  (error, results) => {
+      if(error) {throw error}
+      res.status(200).json(results.rows)
+  })
+})
+
+
 
 module.exports = router;
