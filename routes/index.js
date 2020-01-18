bcea57199e70e9fe0bd981f493903ddf7a635e3b
Wojciech Kubiak <wgkubiak@gmail.com>
2020-01-18 13:00:17 +0100

    Connection update

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = Router();
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  pool.query('SELECT * FROM names',
+  pool.query('SELECT * FROM pigs',
   (error, results) => {
       if(error) {throw error}
       res.status(200).json(results.rows)
--- utility/connection.js
+++ utility/connection.js
@@ -3,11 +3,11 @@ import 'dotenv/config';
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    user: process.env.LOCAL_USER || process.env.HR_USER,
+    host: process.env.LOCAL_HOST || process.env.HR_HOST,
+    database: process.env.LOCAL_NAME || process.env.HR_NAME,
+    password: process.env.LOCAL_PASS || process.env.HR_PASS,
+    port: process.env.LOCAL_PORT || process.env.HR_PORT
 })
 
 module.exports = pool
